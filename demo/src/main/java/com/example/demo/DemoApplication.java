e84d926901364cd272f7ae6bb62149db53608cc4
JackMultani <jackmultani1@gmail.com>
2025-03-23 18:31:54 -0700

    setting up still

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FastAgencyApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FastAgencyApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
